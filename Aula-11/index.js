b10c6e22c816ee1dacbe6e6e8e19c3b39a7c42f1
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-24 15:20:00 -0300

    Create index.js

---
+++ Aula-11/index.js
@@ -0,0 +1,29 @@
+function myFunc(){
+    console.log('hellowwwwww!!')
+
+}
+myFunc();
+
+
+
+function addTowNums(){
+    var a = 10
+    var b = 6
+    var c = a + b;
+    console.log(c)
+}
+addTowNums();
+
+
+function addThreeNums(a,b,c){
+    var d = a + b + c
+    console.log(d)
+}
+addThreeNums(5,6,7);
+
+function addFourNums(a,b,c,d){
+    return a+b+c+d
+    
+}
+
+console.log(addFourNums(1,2,3,4))
