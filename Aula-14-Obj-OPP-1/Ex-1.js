75bfc5e068b85844bfdb698b9cdb800aef7dca10
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-26 23:56:57 -0300

    Update Ex-1.js

--- Aula-14-Obj-OPP-1/Ex-1.js
+++ Aula-14-Obj-OPP-1/Ex-1.js
@@ -24,3 +24,18 @@ meal(dog);
 meal(dog);
 
 console.log(dog.food);
+
+
+function two() {
+    return 2;
+}
+
+function one() {
+    return 1;
+}
+
+function calculate(initialValue, incrementValue) {
+    return initialValue() + incrementValue() + incrementValue();
+}
+
+console.log(calculate(two, one));
