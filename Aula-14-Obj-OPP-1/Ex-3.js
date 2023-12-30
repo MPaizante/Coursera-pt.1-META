5215e3181ae9cc8ce59874a9f3486bd3194d82f5
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-29 21:40:36 -0300

    Update Ex-3.js

--- Aula-14-Obj-OPP-1/Ex-3.js
+++ Aula-14-Obj-OPP-1/Ex-3.js
@@ -12,3 +12,52 @@ class Car{
 const car1 = new Car('red', 120)
 car1.turboOn()
 console.log(car1)
+
+class RJ{
+    zone(){
+        console.log("Oeste, Baixada, Metropolitana, Leste, Sul.")
+    }
+}
+class Sul extends RJ{
+    zone(){
+        console.log("Zone Sul.")
+    }
+    neighborhood1(){
+        console.log("Botafogo!")
+    }
+    neighborhood2(){
+        console.log("Flamengo!")
+    }
+    neighborhood3(){
+        console.log("Copacabana!")
+    }
+    neighborhood4(){
+        console.log("Ipanema!")
+    }
+    
+}
+class Oeste extends RJ{
+    zone(){
+        console.log("Zone Oeste.")
+    }
+    neighborhood1(){
+        console.log("Campo Grande!")
+    }
+    neighborhood2(){
+        console.log("Barra da Tijuca!")
+    }
+    neighborhood3(){
+        console.log("Bangu!")
+    }
+    neighborhood4(){
+        console.log("Cosmos!")
+    }
+    
+}
+
+let O = new Oeste();
+let S = new Sul();
+O.zone();
+O.neighborhood1();
+S.zone();
+S.neighborhood1();
