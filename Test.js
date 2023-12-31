8e6361e34fefb88a24843161ca4d3efca07c78ca
Matheus Paizante <matheus_paizante@hotmail.com>
2023-12-31 00:17:21 -0300

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,12 +1,15 @@
-var age = 10;
-if(age>= 60){
-    console.log("O senhor recebe sua renda da sua pensão")
-}else if(age >65 || age >=18){
-    console.log("Each month you get a salary")
 
-} else if(age <18){
-    console.log("You get an allowance")
-
-}else{
-    console.log("The value of the age variable is not numerical")
+class Person {
+    sayHello() {
+        console.log("Hello");
+    }
 }
+
+class Friend extends Person {
+    sayHello() {
+        console.log("Hey");
+    }
+}
+
+var result = new Friend();
+result.sayHello();
