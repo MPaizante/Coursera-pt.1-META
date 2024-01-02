c4a8e805f45017e500d5301f762c9a562ff3b4fb
Matheus Paizante <matheus_paizante@hotmail.com>
2024-01-02 19:56:10 -0300

    Update Ex-1.js

--- Aula-15/Ex-1.js
+++ Aula-15/Ex-1.js
@@ -41,3 +41,30 @@ console.log(bestBoxers);
 const repetitiveFruits = ['apple','pear','apple','pear','plum', 'apple'];
 const uniqueFruits = new Set(repetitiveFruits);
 console.log(uniqueFruits);
+
+
+
+const x = ['onion', 'garlic', 'potato', 2 , 3 , 4 , 5 , 6]
+
+console.log(...x)
+
+const top7 = [
+    'RJ',
+    'SG',
+    'Niteroi',
+    'Petropolis',
+    'Cabo Frio',
+    'Rio das Ostras',
+    'Duque de Caxias'
+];
+console.log(top7)
+console.log(...top7);
+
+const [] = top7;
+console.log(top7)
+
+function addTaxToPrices(taxRate, ...itemsBought){
+    return itemsBought.map(item => taxRate * item)
+}
+let shoppingCart = addTaxToPrices(1.1,46,89,35,79)
+console.log(shoppingCart)
