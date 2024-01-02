cdc2121edc194d960a95066b49b049f1e6a94e06
Matheus Paizante <matheus_paizante@hotmail.com>
2024-01-02 19:07:36 -0300

    Update Ex-1.js

--- Aula-15/Ex-1.js
+++ Aula-15/Ex-1.js
@@ -30,3 +30,14 @@ droneKeys.forEach( function(key) {
     result.push(key, drone[key])
 })
 console.log(result)
+
+let bestBoxers = new Map();
+bestBoxers.set(1, "The Champion");
+bestBoxers.set(2, "The Runner-up");
+bestBoxers.set(3, "The third place");
+
+console.log(bestBoxers);
+
+const repetitiveFruits = ['apple','pear','apple','pear','plum', 'apple'];
+const uniqueFruits = new Set(repetitiveFruits);
+console.log(uniqueFruits);
