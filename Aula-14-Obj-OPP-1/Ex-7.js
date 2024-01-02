651e3a03b76467cdacb31c0b2308a2aea688fce6
Matheus Paizante <matheus_paizante@hotmail.com>
2024-01-01 21:43:20 -0300

    Update Ex-7.js

--- Aula-14-Obj-OPP-1/Ex-7.js
+++ Aula-14-Obj-OPP-1/Ex-7.js
@@ -37,3 +37,22 @@ var clothingItem = {
 for( const key of Object.keys(clothingItem) ) {
     console.log(key, ":", clothingItem[key])
 }
+
+
+
+function testBracketsDynamicAccess() {
+    var dynamicKey;
+    if(Math.random() > 0.5) {
+      dynamicKey = "speed";
+     }else{
+       dynamicKey = "color";
+     }
+  
+      var drone = {
+        speed: 15,
+        color: "orange"
+      }
+  
+      console.log(drone[dynamicKey]);
+  }
+  testBracketsDynamicAccess();
